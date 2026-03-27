4773f1a0869f44745b630c95e09530ef4667e922
Kurt Croix <205799619+kurt-croix@users.noreply.github.com>
2026-03-27 14:07:35 -0500

    trying to figure out which episode

--- scripts/build-rss.ts
+++ scripts/build-rss.ts
@@ -24,8 +24,8 @@ const PODCAST_KINDS = {
  */
 const IGNORED_EPISODES = [
   'f38a7f8e088ea727e316b990da29cdf8d13352b5fa095941114b83fefa4b67fa:65fae244-4478-41b8-beaf-fca3f345aa60', // Ray County Commissioner's Meeting 3/19
-  'f38a7f8e088ea727e316b990da29cdf8d13352b5fa095941114b83fefa4b67fa:135429e2-f5fd-46fa-a439-a3d8efe6e0b4', // Ray County Commissioner's Meeting 3/19
-  'f38a7f8e088ea727e316b990da29cdf8d13352b5fa095941114b83fefa4b67fa:d6acea6e-7748-4186-92e4-299d0f191273', // Ray County Commissioner's Meeting 3/19 (another version)
+  // 'f38a7f8e088ea727e316b990da29cdf8d13352b5fa095941114b83fefa4b67fa:135429e2-f5fd-46fa-a439-a3d8efe6e0b4', // Ray County Commissioner's Meeting 3/19
+  // 'f38a7f8e088ea727e316b990da29cdf8d13352b5fa095941114b83fefa4b67fa:d6acea6e-7748-4186-92e4-299d0f191273', // Ray County Commissioner's Meeting 3/19 (another version)
 ];
 
 /**
